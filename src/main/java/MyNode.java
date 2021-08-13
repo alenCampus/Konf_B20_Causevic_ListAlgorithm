9d9c3b16f6be4421ea1173c0bc19a3ff7abcad9a
alenCampus <80466222+alenCampus@users.noreply.github.com>
2021-08-13 20:29:43 +0200

    Update MyNode.class with variables and Constructor

--- src/main/java/MyNode.java
+++ src/main/java/MyNode.java
@@ -1,2 +1,37 @@
 public class MyNode {
+    /**
+     * This class represents a structure of a Doubly Linked List
+     * @author: Alen Causevic
+     */
+
+    //-- Instancevariable
+    int value;
+    MyNode nextPointer;
+    MyNode previousPointer;
+
+    /**
+     *
+     * @param: value --> represents the value at the actual Node
+     * @param: nextPointer --> Pointer to the next Value from the list
+     * @param: previousPointer --> Pointer to the previous Value from the list
+     */
+
+
+    /*
+    The Pointer next and previous are currently null because
+    there is no reference to the list right now:
+     */
+
+    //-- Constructor
+    public MyNode(int value){
+        this.value = value;
+        this.nextPointer = null;
+        this.previousPointer = null;
+    }
+
+    /*
+    Representation: || NULL <-> value <-> NULL ||
+     */
+
+
 }
