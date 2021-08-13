c1ef885c6d303a215c21f8814358c87ca77437ee
alenCampus <80466222+alenCampus@users.noreply.github.com>
2021-08-13 23:13:20 +0200

    Update Main.java by creating a list and using Methods from other class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,29 @@
 public class Main {
 
     public static void main(String[] args) {
+        /*
+        The List has to be first of all created by creating an object called "myList".
+        After that the methods from the Class "MyDoubleLinkedList.class" is used to add
+        or print the values from the List.
+         */
+
+        // Implementing a DoublyLinkedList
+    MyDoubleLinkedList myList = new MyDoubleLinkedList();
+
+
+    // Adding Value(s) to our List
+    myList.addValue(myList,10);
+    myList.addValue(myList,20);
+    myList.addValue(myList,25);
+    myList.addValue(myList,30);
+    //----------------------------
+
+        //-- Printing the List
+    myList.printList(myList);
+    //-------
+
+        //-- Analyse the List
+    myList.analyseList(myList);
 
     }
 }
