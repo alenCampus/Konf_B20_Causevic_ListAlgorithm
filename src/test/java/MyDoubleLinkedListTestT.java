0b931b84b88c17a9be4cec7ff256c847ca1dbb8d
alenCampus <80466222+alenCampus@users.noreply.github.com>
2021-08-14 22:32:36 +0200

    Update

--- src/test/java/MyDoubleLinkedListTestT.java
+++ src/test/java/MyDoubleLinkedListTestT.java
@@ -1,4 +1,40 @@
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
 import static org.junit.Assert.*;
+
+@DisplayName("Testing Method -> addValue")
 public class MyDoubleLinkedListTestT {
-  
+    MyNode testNode;
+
+    @Test
+    @DisplayName("Testing if the root value is the first added")
+    public void rootValue() {
+        //Exercise Phase
+        MyDoubleLinkedList l = new MyDoubleLinkedList();
+        l.addValue(l,10); // root --> 10
+        l.addValue(l,20); // Node --> 20
+        l.addValue(l,30); // tail --> 30
+        //Verify Phase
+        assertEquals("The first element of the list is 10.",l.root.value,10);
+    }
+
+    @Test
+    @DisplayName("Testing the last Value of the list")
+    public void tailPointerNull(){
+        // Exercise Phase
+       MyDoubleLinkedList list = new MyDoubleLinkedList();
+       list.addValue(list,10);
+       list.addValue(list,20);
+       list.addValue(list,30); // Expected tailNode to be --> last Element
+       MyNode tailNode = list.root;
+
+       while (tailNode.nextPointer != null){
+           tailNode = tailNode.nextPointer;
+       }
+
+       //Verify Phase
+        assertEquals(tailNode.value,30);
+    }
 }
